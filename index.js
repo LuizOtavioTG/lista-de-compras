d51009061660b7b8275dfc3d8357c8eaf570cc0b
Luiz <bc.luizo@gmail.com>
2025-02-26 16:55:02 -0300

    adicionando mensagem de lista de compras vazia

--- index.js
+++ index.js
@@ -48,4 +48,19 @@ botaoAdicionar.addEventListener("click", (evento) => {
     itemDaLista.appendChild(itemData);
 
     listaDeCompras.appendChild(itemDaLista);
+
+    verificarListaVazia();
 })
+
+const mensagemListaVazia = document.querySelector(".mensagem-lista-vazia");
+
+function verificarListaVazia() {
+    const itensDaLista = listaDeCompras.querySelectorAll("li");
+    if (itensDaLista.length === 0) {
+        mensagemListaVazia.style.display = "block";
+    } else {
+        mensagemListaVazia.style.display = "none";
+    }
+}
+
+verificarListaVazia();
